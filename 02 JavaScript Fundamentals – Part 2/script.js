17a131c5e55b6d57cc5586c4d113b596b879203b
Revolt <revolt@localhost.localdomain>
2021-11-23 16:56:26 +0100

    strict mode. closes #1

---
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+
+if(hasDriversLicense) console.log('i can drive!');
+
+
+//Example: reserved keywords introduced by stric mode
+const interface = 'Audio';
+
+const private = 324;
