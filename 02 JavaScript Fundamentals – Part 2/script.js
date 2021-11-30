6707b38936e467d03a98fb0bb1cbe8490850b80c
Revolt <revolt@localhost.localdomain>
2021-11-30 10:52:10 +0100

    Arrow Function. closes #4

--- 02 JavaScript Fundamentals – Part 2/script.js
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -16,3 +16,19 @@ const calcAge2 = function(birthYear, currentYear){
 const age2 = calcAge2(1978, 2021);
 
 console.log(age1, age2);
+
+//Arrow function
+
+//single line Arrow function 
+const age = birthYear => 2021 - birthYear;
+console.log(age(1996));
+
+//multipe line Arrow function 
+const yearsUntilRetirement = (firstname, birthYear) => {
+
+     const age = 2021-birthYear;
+     const retirement = 65 - age;
+
+     return `${firstname} retires in ${retirement}years`;
+}
+console.log(yearsUntilRetirement('Oddy', 1996));
