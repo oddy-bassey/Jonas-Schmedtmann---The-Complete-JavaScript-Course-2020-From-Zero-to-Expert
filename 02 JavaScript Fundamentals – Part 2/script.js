e1f5d3b19257af5dddc67ee2ea3aa5f28887c092
Revolt <revolt@localhost.localdomain>
2021-12-04 19:05:59 +0100

    Object methods

--- 02 JavaScript Fundamentals – Part 2/script.js
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -1,27 +1,34 @@
 
 //Objects
 
-//Dot vs Bracket notation
+//object function
 const oddy = {
      fristname: 'Oddy',
      lastname: 'Bassey',
-     age: 2021-1996,
-     job: 'CEO Revoltcode',
-     friends: ['Twin', 'Andrew', 'Gabriel', 'Mitchanki']
-};
+     birthYear : '1996',
+     job: 'CEO of Revoltcode',
+     friends: ['Twin', 'Andrew', 'Gabriel', 'Mitchanki'],
 
-const value = prompt('what do u want to know about revoltcode owner(firstname, lastname, friends, age');
+     // age: function(birthYear){
+     //      return 2021-birthYear;
+     // }
+     
+     // age: function(){
+     //      console.log(this);
+     //      return 2021-this.birthYear;
+     // }
 
-if(oddy[value]){
-     console.log(oddy[value]);
-}else{
-     const data = prompt(`provide this information(${value}) about the CEO if you have any idea!`)
-     oddy[value] = data;
-     console.log('revoltcode thanks u :)!');
-}
+     calcAge: function(){
+          this.age = 2021-this.birthYear;
+          return this.age;
+     },
 
-//adding new properties to the object using the Dot & Bracket notation
-oddy.location = 'Denmark';
-oddy['girlfriend'] = 'Uyime';
-console.log(oddy);
-console.log(`${oddy.fristname} has ${oddy.friends.length} friends, and his best friend is ${oddy.friends[1]}`);
+     getSummary: function(){
+          return `${this.fristname} is a ${this.calcAge()} year old ${this.job}, he has ${this.friends.length} friends`;
+     }
+}; 
+
+console.log(oddy.calcAge());
+//console.log(oddy['age'](oddy.birthYear));
+
+console.log(oddy.getSummary());
