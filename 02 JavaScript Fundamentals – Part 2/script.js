04e3f3ebd2a91b2b4b5300a9e48fe305f06eedd8
Revolt <revolt@localhost.localdomain>
2021-12-11 22:43:20 +0100

    setting up prettier and vscode

--- 02 JavaScript Fundamentals – Part 2/script.js
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -1,7 +1,7 @@
 //LOOPS
 
-for (let i=1; i<=10; i++){
-     //console.log(`running ${i} times!`)
+for (let i = 1; i <= 10; i++) {
+  //console.log(`running ${i} times!`)
 }
 
 // continue and break stateents
@@ -9,27 +9,28 @@ for (let i=1; i<=10; i++){
 //continue: it exists the current iteration of  the loop and moves to the next iteration
 // break: exists from the loop entirely
 
-for (let i=1; i<=10; i++){
-     //if(i===5) continue;
+for (let i = 1; i <= 10; i++) {
+  //if(i===5) continue;
 
-     if(i===5) break;
-     console.log(`running ${i} times!`)
+  if (i === 5) break;
+  console.log(`running ${i} times!`);
 }
 
 let iteration = 1;
 
 //WHILE LOOP
-// while (iteration <= 10){ 
+// while (iteration <= 10){
 //      console.log(`while loop executing ${iteration} times!`)
 
 //      iteration++;
 // }
 
-let dice = Math.trunc(Math.random()*6)+1; 
+let dice = Math.trunc(Math.random() * 6) + 1;
 console.log(dice);
 
-while (dice !== 6){ 
-     console.log(`dice ${dice}`);
+while (dice !== 6) {
+  console.log(`dice ${dice}`);
 
-     dice = Math.trunc(Math.random()*6)+1; 
+  dice = Math.trunc(Math.random() * 6) + 1;
 }
+const data = (name) => '' + name;
