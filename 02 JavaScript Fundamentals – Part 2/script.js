33bff0d109814867a16866c7c2003b7bc9e81ec5
Revolt <revolt@localhost.localdomain>
2021-12-26 11:42:03 +0100

    destructuring arrays

--- 02 JavaScript Fundamentals – Part 2/script.js
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -1,119 +1,51 @@
 'use strict';
 
-//declaring state variables
-let scores, currentScore, activePlayer, isGamePlaying;
+const arr = [21, 32, , 43];
+console.log(arr);
 
-// selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const [x, y, z] = arr;
+console.log(x, y, x);
 
-const diceEl = document.querySelector('.dice');
-const newButton = document.querySelector('.btn--new');
-const rollButton = document.querySelector('.btn--roll');
-const holdButton = document.querySelector('.btn--hold');
-
-// starting conditions
-init();
-
-// Rolling dice functionality
-rollButton.addEventListener('click', function () {
-  if (isGamePlaying) {
-    // 1. Generating a random dice roll
-    let dice = getRandomDice() + 1;
-    console.log(dice, 'was rolled');
-
-    // 2. Display dice
-    displayDice(false);
-    diceEl.src = `dice-${dice}.png`;
-
-    // 3. Check for rolled 1: if true, switch to next player
-    if (dice !== 1) {
-      // Add dice to the current score
-      currentScore += dice;
-      setPlayerCurrentScore(activePlayer, currentScore);
-    } else {
-      // Switch to the next player
-      switchPlayer();
-    }
-  }
-});
-
-holdButton.addEventListener('click', function () {
-  if (isGamePlaying) {
-    // 1. Add score to active player's score
-    scores[activePlayer] += currentScore;
-    setPlayerScore(activePlayer, scores[activePlayer]);
-
-    // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
-      //Finnish the game
-      isGamePlaying = false;
-
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
-    } else {
-      //Switch to next player
-      switchPlayer();
-    }
-  }
-});
-
-newButton.addEventListener('click', init);
-
-let getRandomDice = function () {
-  return Math.trunc(Math.random() * 6);
+const restraunt = {
+  name: 'African cousine',
+  location: 'Akwa-Ibom',
+  categories: ['ibibio', 'igbo', 'hausa', 'yoruba', 'efik '],
+  startMenu: [
+    'rice',
+    'affang',
+    'chinkafa',
+    'akamu',
+    'beans',
+    'ogbono',
+    'salad',
+  ],
+  mainMenu: ['jolof', 'moi moi', 'palm cake', 'wine'],
+  order: function (startIndex, mainIndex) {
+    return [this.startMenu[startIndex], this.mainMenu[mainIndex]];
+  },
 };
 
-let switchPlayer = function () {
-  setPlayerCurrentScore(activePlayer, 0);
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  currentScore = 0;
+//destructuring arrays
+let [cat1, , cat2] = restraunt.categories;
+console.log(cat1, cat2);
 
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-};
+//swapping during destructuring
+[cat1, , cat2] = [cat2, , cat1];
+console.log(cat1, cat2);
 
-function setPlayerCurrentScore(player, score) {
-  document.getElementById(`current--${player}`).textContent = score;
-}
+const [starterMeal, mainMeal] = restraunt.order(2, 0);
+console.log(starterMeal, mainMeal);
 
-function setPlayerScore(player, score) {
-  document.getElementById(`score--${player}`).textContent = score;
-}
+//destructuring nested array
+const nested = [2, 4, [5, 6]];
+const [i, , j] = nested;
 
-function displayDice(isDisplay) {
-  if (isDisplay) {
-    diceEl.classList.add('hidden');
-  } else {
-    diceEl.classList.remove('hidden');
-  }
-}
+console.log(i, j);
 
-function init() {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  isGamePlaying = true;
+const [k, l, [m, n]] = nested;
+console.log(k, l, m, n);
 
-  displayDice(true);
+//deafault values
+const [p = 1, q = 1, r = 1] = [9, 8];
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-}
+console.log(p, q, r);
