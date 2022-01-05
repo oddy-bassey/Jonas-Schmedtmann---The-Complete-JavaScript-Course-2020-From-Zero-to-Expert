67d1fe17917b5a157446bbeab0ab11bb1d283655
Revolt <revolt@localhost.localdomain>
2022-01-05 10:56:24 +0100

    short circuiting (&& and ||)

--- 02 JavaScript Fundamentals – Part 2/script.js
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -38,54 +38,14 @@ const restraunt = {
   },
 };
 
-const arr = [3, 4, 5, 6];
-const newArr = [2, 2, ...arr];
+// SHORT CIRCUITING
 
-console.log(newArr);
-console.log(...newArr);
+// OR operator: short circuits when the first value is true (returns the first truity value)
+console.log(undefined || 'jonas');
+console.log(undefined || 'jonas' || 0 || 1 || true);
 
-const newMenu = [...restraunt.mainMenu, 'epkankukwo'];
-console.log(newMenu);
+const guest = restraunt.numGuests || 10;
+console.log(guest);
 
-// USES OF SPREAD OPERATOR
-
-// copy array (shallow copy)
-const mainMenuCopy = [...restraunt.mainMenu];
-
-// merge arrays
-const menu = [...restraunt.mainMenu, ...restraunt.startMenu];
-console.log(mainMenuCopy, menu);
-
-// Iterables: arrays, strings, maps, sets
-const name = 'Oddy Bassey';
-const letters = [...name];
-console.log(letters);
-
-// creating a shallow copy of the object
-const restrauntCopy = { ...restraunt };
-console.log(restrauntCopy);
-
-// Rest pattern, because on left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, others);
-
-const [pizza, risoto, ...otherFood] = [
-  ...restraunt.mainMenu,
-  ...restraunt.startMenu,
-];
-
-console.log(pizza, risoto, otherFood);
-
-const { sat, ...weekDays } = restraunt.openningHours;
-console.log(sat, weekDays);
-
-const add = function (...numbers) {
-  // (...numbers) -> rest parameter
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-
-  console.log(`total sum ${sum}`);
-};
-
-const x = [3, 6, 7, 4, 7, 3, 54, 332];
-add(...x);
+// AND operator: short circuits when the first value is false (returns the first falsy value)
+console.log(1 && 'jonas');
