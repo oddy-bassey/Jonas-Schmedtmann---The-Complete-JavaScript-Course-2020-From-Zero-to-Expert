10ffdf5493f2aaf78f45c6d0247bc406d653bd6c
Revolt <revolt@localhost.localdomain>
2022-01-12 13:48:37 +0100

    enhanced object literals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,51 +1,49 @@
-'strict-mode';
+'use strict';
+
+// Enhanced object literals
+
+const players = [
+  [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnarby',
+    'Lewandowski',
+  ],
+  [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+  ],
+];
+
+const teams = ['team1', 'team2'];
 
 const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
+  [teams[0]]: 'Bayern Munich', // computing property names using destructuring
+  [`team${1 + 1}`]: 'Borrussia Dortmund', // computing property names using string combination and math operations
   score: '4:0',
   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
   date: 'Nov 9th, 2037',
+  players, // defining object property using variable
   odds: { team1: 1.33, x: 3.25, team2: 6.5 },
+
+  //defining object funtion using simple method declaration pattern
+  teamInfo() {
+    console.log(`we have ${this.team1} & ${this.team2} present!`);
+  },
 };
-
-// the for-of loop
-const [bayernTeam] = game.players;
-
-for (const player of bayernTeam) console.log(player);
-
-for (const player of bayernTeam.entries()) {
-  console.log(player);
-}
-
-for (const [index, player] of bayernTeam.entries()) {
-  console.log(`${index} ${player}`);
-}
