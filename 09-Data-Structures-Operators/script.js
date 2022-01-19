f4bac7fc7b805c34e9a84cb30aea11d84230d4aa
Revolt <revolt@localhost.localdomain>
2022-01-19 10:29:38 +0100

    coding challenge, tut 114

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,51 +1,64 @@
-'use strict';
-
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thurs: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order(startIndex, mainIndex) {
-    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
-  },
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: { team1: 1.33, x: 3.25, team2: 6.5 },
 };
 
-// object properties || keys
-const properties = Object.keys(restaurant.openingHours);
-
-let openStr = `we are open on ${properties.length} days: `;
-for (const day of properties) {
-  openStr += `${day},`;
+// Question 1
+for (const [goal, player] of game.scored.entries()) {
+  console.log(`${player} goal ${goal + 1}`);
 }
-console.log(openStr);
 
-// property values
-const values = Object.values(restaurant.openingHours);
-console.log(values);
-
-// entire object || entries
-const entries = Object.entries(restaurant.openingHours);
-console.log(entries);
-
-// [key, value]
-for (const [key, { open, close }] of entries) {
-  console.log(`on ${key} we open at ${open} and close at ${close}`);
+// Question 2
+let totalOdd = 0;
+const oddValues = Object.values(game.odds);
+for (const odd of oddValues) {
+  totalOdd += odd;
 }
+const averageOdd = totalOdd / oddValues.length;
+console.log(`Average odd is ${averageOdd}`);
+
+// Question 3
+const oddEntries = Object.entries(game.odds);
+for (const [team, odd] of oddEntries) {
+  let clause = team === 'x' ? 'draw' : `${game[team]} victory`;
+  console.log(`Odd of  ${clause} ${odd}`);
+}
+
+// Bonus question
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
