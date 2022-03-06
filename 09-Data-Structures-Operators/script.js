7106f9b26e77470e093edde09c08cc7e43797fe6
Revolt <revolt@localhost.localdomain>
2022-03-06 17:56:12 +0100

    map iteration

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -7,7 +7,7 @@ const question = new Map([
   [1, 'C'],
   [2, 'Java'],
   [3, 'Javascript'],
-  ['Correct', 3],
+  ['Correct', 2],
   [true, 'Correct'],
   [false, 'Try Again'],
 ]);
@@ -22,3 +22,22 @@ const person = {
 
 const personMap = new Map(Object.entries(person));
 console.log(personMap);
+
+// looping through a map
+console.log(question.get('Question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key} : ${value}`);
+  }
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('C orrect') === answer));
+
+// converting a map back to an Array
+console.log([...question]);
+//console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
