ab26b234af6a230271948e2f20e661360c19d86b
oddy-bassey <oddybassey@gmail.com>
2022-05-04 06:13:08 +0100

    call, apply & bind method

--- course-work/script.js
+++ course-work/script.js
@@ -1,37 +1,103 @@
 'use strict'
 
+const lufthansa = {
+    airline : 'Lufthansa',
+    iataCode : 'LH',
+    bookings : [],
+    book(flightNum, name){
+        console.log(`${name} booked a seat on ${this.airline} flight 
+            ${this.iataCode}${flightNum}`);
+
+        this.bookings.push({flight : `${this.iataCode}${flightNum}`, name});
+    }
+}
+
+lufthansa.book(235, 'Jonas Schmedtmann');
+lufthansa.book(567, 'Jonas Smith');
+console.log(lufthansa);
+
+const eurowings = {
+    airline : "Eurowings",
+    iataCode : 'EW',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work
+// book(23, 'Sarah Williams')
+
 /*
- * NOTE: 
- * 
- * First Class Functions: this is simply a concept that functions are
- * values or other object types'
- * 
- * Higher Order Functions: these are functions that receive as parameter
- * a function or returns a funtion
+ * using call, apply & bind method to explicitly define the "this" keyword
+ * in any function that we want
 */
 
-/*
-const greet = function(greeting){
-    return function(name){
-        console.log(`${greeting} 😉 ${name}`);
-    }
-}*/
+// Call method
+book.call(eurowings, 345, 'Sarah Williams')
+console.log(eurowings);
 
-// re-writing the above function (greet) using arrow functions
+book.call(lufthansa, 555, 'Abu Dhabi')
+console.log(lufthansa);
 
-/*
-const greet = (greeting) => {
-    return (name) => {
-        console.log(`${greeting} 😉 ${name}`);
-    }
-}*/
+const swiss = {
+    airline : "Swiss Airlines",
+    iataCode : 'LX',
+    bookings: [],
+}
 
-// OR
-const greet = greeting => name => console.log(`${greeting} 😉 ${name}`);
+book.call(swiss, 599, "Mary Swagger");
+console.log(swiss);
 
-const greeter = greet('Hey');
-greeter('Oddy');
-greeter('Etap');
-greeter('Danny');
+// Apply method 
+// this isn't used anymore in modern JS because there's a better way of doing the exact same thing (using call method)
+const flightData = [577, "George copper"];
+book.apply(swiss, flightData);
+console.log(swiss);
 
-greet('Hello') ('Banky');
+book.call(swiss, ...flightData);
+
+// Bind method
+/* NOTE: "bind" does not immediately call the function, instead it returns a new function where the "this" keyword is bound
+ * It is set to whatever value we pass
+*/
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, "Steven Williams");
+bookLH(23, "Mark Hendry");
+bookLX(23, "Judge Trump");
+
+/* set in stone certain function arguements
+ * Note: setting parts of the arguement before hand is also known as partial application
+*/
+const bookEW23 = book.bind(eurowings, 77);
+bookEW23("Richard Crosbie");
+
+// other situations where the bind method is used: with Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function(){
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+}
+
+lufthansa.buyPlane();
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// other situations where the bind method is used: Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+// or
+const addVatTax = addTax.bind(null, 0.1);
+console.log(addVatTax(220));
+
+// challenge: write a function that returns a function like the addVatTax 
+function addTaxRate(rate){
+    return (value) => value + value * rate;
+}
+
+const addVat2 = addTaxRate(0.1);
+console.log(addVat2(200));
